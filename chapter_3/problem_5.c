7c30dc514a73637b6503421fed9e152501de32be
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-01-01 13:51:33 +0900

    Create problem_5.c

---
+++ chapter_3/problem_5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdilb.h>
+#include <math.h>
+
+int main() {
+    int number = 0, digit[10], numofdigits, i;
+    system("cls");
+  
+    printf("\n Enter the number of digits : ");
+    scanf_s("%d", &numofdigits);
+  
+    for (i = 0; i < numofdigits; i++) {
+        printf("\n Enter the digit at position %d : ", i + 1);
+        scanf_s("%d", &digit[i]);
+    }
+    i = 0;
+    while(i < numofdigits) {
+        number = number + digit[i] * pow(10, i);
+        i++;
+    }
+    printf("\n The number is %d", number);
+  
+    return 0;
+}
