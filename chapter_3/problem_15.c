1ea2f3ad0dfdb8e24f74011b6a650615811baa8c
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-01-03 15:45:14 +0900

    Create problem_15.c

---
+++ chapter_3/problem_15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int* ptr1, ptr2;
+    ptr1 = arr;
+    ptr2 = &arr[8];
+    
+    while(ptr1 <= ptr2) {
+        print("%d", *ptr1);
+        ptr1++;
+    }
+    return 0;
+}
