8a03d27407ce88dca43713c5009da8b3e7e29222
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-01-04 12:00:02 +0900

    Create problem_26.c

---
+++ chapter_3/problem_26.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int array[2][2][2], i, j, k;
+    printf("\n Enter the elements of the matrix");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            for(k = 0; k < 2; k++)
+                scanf("%d", &array[i][j][k]);
+        }
+    }
+
+    printf("\n The matrix is : ");
+    for(i = 0; i < 2; i++) {
+        printf("\n");
+        for(j = 0; j < 2; j++) {
+            printf("\n");
+            for(k = 0; k < 2; k++)
+                printf("\t array[%d][%d][%d] = %d", i, j, k, array[i][j][k]);
+        }
+    }
+    getch();
+    return 0;
+}
