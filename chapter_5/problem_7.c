6cb8433751530b9279211f7bbfd3feb99346ee10
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-01-19 17:23:46 +0900

    Create problem_7.c

---
+++ chapter_5/problem_7.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student {
+        int r_no;
+        char name[20];
+        char course[20];
+        int fees;
+};
+struct student* ptr_stud[10];
+
+int main() {
+    int i, n;
+  ----------------수정 필요----------------------
+    struct student stud1, *ptr_stud1;
+    system("cls");
+
+    ptr_stud1 = &stud1;
+  
+    printf("\n Enter the details of the student : ");
+    printf("\n Enter the Roll Number = ");
+    scanf("%d", &ptr_stud1->r_no);
+    
+    gets(ptr_stud1->name);
+    printf("\n Enter the Name = ");
+    gets(ptr_stud1->name);
+
+    printf("\n Enter the Course = ");
+    gets(ptr_stud1->course);
+  
+    printf("\n Enter the Fees = ");
+    scanf("%d", &ptr_stud1->fees);
+
+    printf("\n DETAILS OF THE STUDENT ");
+    printf("\n ROLL NUMBER = %d", ptr_stud1->r_no);
+    printf("\n NAME = %s", ptr_stud1->name);
+    printf("\n COURSE = %s", ptr_stud1->course);
+    printf("\n FEES = %d", ptr_stud1->fees);
+    
+    return 0;
+}
