5703d16217fa62a0bfed4db0d3e73479ab5e2d8c
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-01-21 18:58:14 +0900

    problem_25.c 업데이트

--- chapter_3/problem_25.c
+++ chapter_3/problem_25.c
@@ -7,9 +7,8 @@ int main() {
     int i, j, mat[3][3];
     printf("\n Enter the elements of the matrix");
     for(i = 0; i < 3; i++) {
-        for(j = 0; j < 3; j++) {
+        for(j = 0; j < 3; j++)
             scanf("%d", mat[i][j]);
-        }
     }
     display(mat);
     return 0;
