81f0bfa4ce86d98e7be31dd3b48289b1f75f52d2
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-02-02 10:34:08 +0900

    Create problem_3.c

---
+++ chapter_7/problem_3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+        char val[] = { 'a', 'b', '+' };
+        char temp;
+
+        printf("\n---BEFORE\n");
+        for(int i = 0; i < 3; i++) {
+              printf("\t%c", val[i]);
+        }
+
+        temp = val[0];
+        val[0] = val[2];
+        val[2] = temp;
+
+        printf("\n---AFTER\n");
+        for(int i = 0; i < 3; i++) {
+              printf("\t%c", val[i]);
+        }
+}
