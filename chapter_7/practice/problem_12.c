62a253667b5a3f65044a75881f1cbee3e2c491a9
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-02-26 10:55:56 +0900

    Create problem_12.c

---
+++ chapter_7/practice/problem_12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int exp_rec(int, int);
+
+int main() {
+          int num1, num2, res;
+
+          printf("\nEnter the two numbe : ");
+          scanf("%d %d", &num1, &num2);
+
+          res = exp_rec(num1, num2);
+          printf("\n RESULT = %d", res);
+  
+          return 0;
+}
+int exp_rec(int x, int y) {
+          if(y == 0)
+                    return 1;
+          else 
+                    return (x * exp_rec(x, y - 1));
+}
