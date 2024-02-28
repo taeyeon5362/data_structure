23e30f08bb434dd752b6d766cc258943f3288d96
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2024-02-28 19:11:24 +0900

    problem_13.c 업데이트

--- chapter_7/practice/problem_13.c
+++ chapter_7/practice/problem_13.c
@@ -20,5 +20,6 @@ int Fibonacci(int n) {
                     return 0;
           else if(n == 1)
                     return 1;
-          else return (Fibonacci(n - 1) + Fibonacci(n - 2));
+          else 
+                    return (Fibonacci(n - 1) + Fibonacci(n - 2));
 }
